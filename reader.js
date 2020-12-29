4bdbed9d23b075ed30ec2ce4af1f1f3dd3f67a11
robimalco <robimalco@gmail.com>
2020-12-29 22:40:18 +0100

    first commit

---
+++ reader.js
@@ -0,0 +1,34 @@
+var injectScript = `
+  document.getElementsByTagName(
+    "news-app"
+  )[0].shadowRoot.querySelector(
+    "iron-pages"
+  ).querySelector(
+    "news-article"
+  ).shadowRoot.querySelector(
+    "div > div > amp-viewer > div"
+  ).shadowRoot.querySelector(
+    "body > main > article"
+  ).querySelectorAll(
+    ".detail-article_container"
+  )[2].querySelector(
+    "div > .paywall"
+  ).innerText.split(
+    String.fromCharCode(160))[0];
+`
+
+document.addEventListener('DOMContentLoaded', function() {
+  var checkPageButton = document.getElementById('readPage');
+  checkPageButton.addEventListener('click', function() {
+    chrome.tabs.getSelected(null, function(tab) {
+      chrome.tabs.executeScript({
+        code: injectScript,
+      }, receiveText);
+    });
+  }, false);
+}, false);
+
+function receiveText(resultsArray){
+  document.body.style.fontSize = "x-large";
+  document.body.innerHTML = resultsArray[0];
+}
